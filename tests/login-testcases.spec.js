5fca34ca12535a3acacee9c63ddd5ae8bea09ebe
Muhammad Ali <m.ali.saif12@gmail.com>
2024-10-21 21:59:06 +0500

    Removed unnecessary files

--- tests/login-testcases.spec.js
+++ tests/login-testcases.spec.js
@@ -2,15 +2,19 @@ import { expect, test } from "@playwright/test";
 import { LoginPage } from "../pages/LoginPage";
 
 test.use({ storageState: { cookies: [], origins: [] } });
+var login;
 
 test("LoginWith Valid credentials", async({page}) =>{
     try{
-    const login = new LoginPage(page)
+    login = new LoginPage(page)
     await login.goto(process.env.baseUrl);
     await login.login(process.env.email,process.env.password);
     await expect(login.page).toHaveTitle("My Account");
     }
     catch(ex){
         console.error(ex)
+    }
+    finally{
+        login.page.close()
     }
 })
