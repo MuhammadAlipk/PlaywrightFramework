e0144d42d8b4687b63703259f0b0a07d16861e5a
MuhammadAlipk <m.ali.saif12@gmail.com>
2024-10-24 17:37:22 +0500

    Tablet page is added

---
+++ pages/TabletPage.js
@@ -0,0 +1,11 @@
+import { BasePage } from "./BasePage";
+
+export class TabletPage extends BasePage {
+  /**
+   * @param {import(playwright/test).Page} page
+   */
+
+  constructor(page) {
+    super(page);
+  }
+}
