fc13412df1c4133a28831ea140fb106bc639f04f
MuhammadAlipk <m.ali.saif12@gmail.com>
2024-11-12 18:17:19 +0500

    Soping testcases file is add along with the add to cart functionality

--- pages/TabletPage.js
+++ pages/TabletPage.js
@@ -1,3 +1,4 @@
+import { constants } from "buffer";
 import { BasePage } from "./BasePage";
 
 export class TabletPage extends BasePage {
@@ -7,17 +8,28 @@ export class TabletPage extends BasePage {
 
   constructor(page) {
     super(page);
+    this.page = page;
+    this.successLabel = this.page.locator("//*[@id='product-category']/div[1]");
+  }
+  async gotoPhonesPage() {
+    await this.goto(process.env.baseUrl);
+    await this.gotoProductPage("Phones & PDAs");
   }
 
-  async retruntProductButton(name) {
-    var product = await this.page.locator(".caption");
-    const index = 0;
-    for (let i = 0; i < product.lenth; i++) {
-      if (product[i].filter({ hasText: name })) {
-        index = i;
-        break;
+  async addToCart(name) {
+    const product = await this.page.locator(".caption h4 a").all();
+    var index = 0;
+    if (product.length > 0) {
+      for (let i = 0; i < product.length; i++) {
+        if ((await product[i].innerText()) == name) {
+          index = i;
+          break;
+        }
       }
     }
-    return await getByRole("button", { name: " Add to Cart" }).nth(index);
+    await this.page
+      .getByRole("button", { name: "Add to Cart" })
+      .nth(index)
+      .click();
   }
 }
